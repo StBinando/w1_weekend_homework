f4d09c2fd9f11bb8e0f7ba25e4764a5016f0db05
Stefano Binando <sbinando@gmail.com>
2021-10-22 09:20:35 +0100

    Fifth function ok - amends 1 parameter name in function 3

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -6,9 +6,11 @@ def get_pet_shop_name(pet_shop):
 def get_total_cash(pet_shop):
     return pet_shop["admin"]["total_cash"]
 
-def add_or_remove_cash(pet_shop, amount):
-    pet_shop["admin"]["total_cash"] += amount
+def add_or_remove_cash(pet_shop, amount_of_money):
+    pet_shop["admin"]["total_cash"] += amount_of_money
 
 def get_pets_sold(pet_shop):
     return pet_shop["admin"]["pets_sold"]
 
+def increase_pets_sold(pet_shop, number_of_pets):
+    pet_shop["admin"]["pets_sold"] += number_of_pets
